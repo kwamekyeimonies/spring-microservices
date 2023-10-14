d08eb0f384d409b65eabc2ca117a45f2598606a2
kwamekyeimonies <kwamekyeimonies@gmail.com>
2023-10-14 23:06:05 +0000

    inverntory service implementation

---
+++ inventoryservice/src/main/java/com/inventoryservice/inventoryservice/InventoryserviceApplication.java
@@ -0,0 +1,34 @@
+package com.inventoryservice.inventoryservice;
+
+import com.inventoryservice.inventoryservice.model.Inventory;
+import com.inventoryservice.inventoryservice.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class InventoryserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryserviceApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository){
+		return args -> {
+			Inventory inventory = new Inventory();
+			inventory.setSkuCode("AlienWare_2023");
+			inventory.setQuantity(99);
+
+			Inventory inventoryy = new Inventory();
+			inventoryy.setSkuCode("Playstation");
+			inventoryy.setQuantity(100);
+
+			inventoryRepository.save(inventory);
+			inventoryRepository.save(inventoryy);
+
+		};
+	}
+
+}
