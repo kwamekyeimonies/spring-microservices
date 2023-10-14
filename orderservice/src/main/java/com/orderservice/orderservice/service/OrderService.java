89ad24867f20dd7535ad8c39c45c726105fdc626
kwamekyeimonies <kwamekyeimonies@gmail.com>
2023-10-14 23:32:02 +0000

    staging of project

---
+++ orderservice/src/main/java/com/orderservice/orderservice/service/OrderService.java
@@ -0,0 +1,42 @@
+package com.orderservice.orderservice.service;
+
+
+import com.orderservice.orderservice.dto.OrderLineItemsDto;
+import com.orderservice.orderservice.dto.OrderRequest;
+import com.orderservice.orderservice.model.Order;
+import com.orderservice.orderservice.model.OrderLineItems;
+import com.orderservice.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    public void placeOrder(OrderRequest orderRequest){
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItems> orderLineItemsList = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToDto)
+                .toList();
+
+        order.setOrderLineItemsList(orderLineItemsList);
+
+        orderRepository.save(order);
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+
+        return orderLineItems;
+    }
+}
---
+++ productservice/src/main/java/com/springmicroservices/springmicroservices/SpringmicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.springmicroservices.springmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmicroservicesApplication.class, args);
+	}
+
+}
