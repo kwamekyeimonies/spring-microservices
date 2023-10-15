a3f9a317e9e081b1799ad060d57765280deabd2c
kwamekyeimonies <kwamekyeimonies@gmail.com>
2023-10-15 22:16:14 +0000

    eureka server & client integration

--- productservice/src/main/java/com/springmicroservices/springmicroservices/SpringmicroservicesApplication.java
+++ productservice/src/main/java/com/springmicroservices/springmicroservices/SpringmicroservicesApplication.java
@@ -2,8 +2,10 @@ package com.springmicroservices.springmicroservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringmicroservicesApplication {
 
 	public static void main(String[] args) {
