9171c012747e510ec68190945bd5cc1aafe00902
kwamekyeimonies <kwamekyeimonies@gmail.com>
2023-10-17 13:50:41 +0000

    security implementation

--- productservice/src/main/java/com/springmicroservices/springmicroservices/repository/ProductRepository.java
+++ productservice/src/main/java/com/springmicroservices/springmicroservices/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.springmicroservices.springmicroservices.repository;
 
 import com.springmicroservices.springmicroservices.models.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends MongoRepository<Product,String> {
 }
