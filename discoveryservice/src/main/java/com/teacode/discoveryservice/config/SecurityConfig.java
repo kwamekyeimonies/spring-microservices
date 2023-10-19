d3c0b1358cf794cc861a327274545bfd00c09900
kwamekyeimonies <kwamekyeimonies@gmail.com>
2023-10-19 16:26:36 +0000

    spring web-flux & websecurity deprecated modules fixes

--- discoveryservice/src/main/java/com/teacode/discoveryservice/config/SecurityConfig.java
+++ discoveryservice/src/main/java/com/teacode/discoveryservice/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.teacode.discoveryservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -7,19 +8,27 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig  {
+
+    @Value("${eureka.username}")
+    private String username;
+
+    @Value("${eureka.password}")
+    private String password;
     @Bean
     public InMemoryUserDetailsManager inMemoryUserDetailsManager(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
 
        authenticationManagerBuilder.
                inMemoryAuthentication()
-               .withUser("eureka").password("password")
-               .authorities("USER");
+               .withUser(username).password(password)
+               .roles("USER");
+//               .authorities();
 
        return (InMemoryUserDetailsManager) authenticationManagerBuilder.build();
     }
