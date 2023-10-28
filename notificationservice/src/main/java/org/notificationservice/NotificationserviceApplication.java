6eeb1d84d655906ad296d96e93ce08d913168b37
kwamekyeimonies <kwamekyeimonies@gmail.com>
2023-10-28 15:15:06 +0000

    consumer notification service dev

--- notificationservice/src/main/java/org/notificationservice/NotificationserviceApplication.java
+++ notificationservice/src/main/java/org/notificationservice/NotificationserviceApplication.java
@@ -1,13 +1,23 @@
 package org.notificationservice;
 
+import lombok.extern.slf4j.Slf4j;
+import org.notificationservice.event.OrderPlaceEvent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
 
 @SpringBootApplication
+@Slf4j
 public class NotificationserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationserviceApplication.class, args);
 	}
+
+	@KafkaListener(topics = "notification")
+	public void handleNotification(OrderPlaceEvent orderPlaceEvent){
+		log.info("Receiveed Notification for order - {}",orderPlaceEvent.getOrderNumber());
+	}
+
 
 }
