457d00c622ed61eabdb40907de0daf97220585c1
kwamekyeimonies <kwamekyeimonies@gmail.com>
2023-10-29 12:53:33 +0000

    kafka integration into notification service

--- notificationservice/src/main/java/org/notificationservice/config/KafkaConsumerConfig.java
+++ notificationservice/src/main/java/org/notificationservice/config/KafkaConsumerConfig.java
@@ -2,18 +2,54 @@ package org.notificationservice.config;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.notificationservice.event.OrderPlaceEvent;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
-@Configuration(proxyBeanMethods = false)
-@RequiredArgsConstructor
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration()
 public class KafkaConsumerConfig {
 
-    private final ConcurrentKafkaListenerContainerFactory concurrentKafkaListenerContainerFactory;
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServer;
 
-    @PostConstruct
-    void setup(){
-        this.concurrentKafkaListenerContainerFactory.getContainerProperties().setObservationEnabled(true);
+    public Map<String, Object>consumerConfig(){
+        Map<String,Object> properties = new HashMap<>();
+
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapServer);
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, OrderPlaceEvent.class);
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,OrderPlaceEvent.class);
+
+        return properties;
+    }
+
+    @Bean
+    public  ConsumerFactory<String,OrderPlaceEvent> consumerFactory(){
+        return new DefaultKafkaConsumerFactory<>(consumerConfig());
+    }
+
+    @Bean
+    public KafkaListenerContainerFactory<
+            ConcurrentMessageListenerContainer<String,OrderPlaceEvent>> factory(
+            ConsumerFactory<String,OrderPlaceEvent> consumerFactory
+    ){
+
+        ConcurrentKafkaListenerContainerFactory<String,OrderPlaceEvent> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+
+        return factory;
     }
 
 }
--- notificationservice/src/main/java/org/notificationservice/event/OrderPlaceEvent.java
+++ notificationservice/src/main/java/org/notificationservice/event/OrderPlaceEvent.java
@@ -1,6 +1,7 @@
 package org.notificationservice.event;
 
-import zlombok.AllArgsConstructor;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
